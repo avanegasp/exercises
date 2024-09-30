af40c8f96e5d66dcad5a3dfb2d54204be769dff3
avanegas <arco527@gmail.com>
2024-09-30 09:15:25 -0500

    Add evernum

--- app.py
+++ app.py
@@ -1,18 +1,14 @@
-num = 1000
-new_num = []
+# Encuentra los números pares
+# Escribe una función que reciba un número y retorne todos los números pares
+# desde 0 hasta ese número.
 
-new_num = [n < 1000 for n in num]
-new_num = 0
 
-for n in range(num, 0, -1):
-    if n % 3 == 0 and n % 5 == 0:
-        new_num.append(n)
-print(new_num)
 
-while num > 0:
-    if num % 3 == 0 and num % 5 == 0:
-        print(num)
-    num -= 1
+def ever_num(num):
+    newnum = []
+    for num in range(num + 1):
+        if num % 2 == 0:
+            newnum.append(num)
+    return newnum
 
-new_num = [n for n in range(num) if n % 3 == 0 or n % 5 == 0]
-print(new_num)
+print(ever_num(1000))
