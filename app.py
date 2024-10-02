82d9a9a52f8726c6a36827780fcca4e5a9e31fe5
avanegas <arco527@gmail.com>
2024-10-02 14:58:26 -0500

    Add comprehension list palindromo

--- app.py
+++ app.py
@@ -10,11 +10,35 @@
 #             new_ever.append(i)
 #     return new_ever
 
-def ever_num(num):
-    new_ever = [i for i in range(0, num, 1) if i % 2 == 0]
-    return new_ever
+# def ever_num(num):
+#     new_ever = [i for i in range(0, num, 1) if i % 2 == 0]
+#     return new_ever
+#
+# print(ever_num(20))
 
-print(ever_num(20))
+#
+# Número Palíndromo
+# Dado un número, crea una función que determine si es un número palíndromo
+# (se lee igual de izquierda a derecha que de derecha a izquierda).
+
+pals = [3,55,67,322,1221,7889,565]
+
+# def palindromo(nums):
+#     new_pal = []
+#
+#     for num in nums:
+#         if str(num) == str(num)[::-1]:
+#             new_pal.append(num)
+#     return new_pal
+#
+# print(palindromo(pals))
+
+def palindromo(nums):
+    new_pal = [num for num in nums if str(num) == str(num)[::-1]]
+    return new_pal
+
+print(palindromo(pals))
+
 
 
 
